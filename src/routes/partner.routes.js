62a63c845ba631e748c6717bdf50913b8498863a
MuriloMarquesSantos <murilommms@gmail.com>
2021-02-19 20:18:52 -0300

    Adding dotenv to make configuration easier

--- src/routes/partner.routes.js
+++ src/routes/partner.routes.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (request, response) => {
+const PARTNERS_URI_PREFIX = process.env.PARTNERS_URI_PREFIX
+
+router.get(`${PARTNERS_URI_PREFIX}`, (request, response) => {
     return response.status(200).json({success: 'true'})
 })
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routes = require('./routes/partner.routes')
+const path = require('path');
 
 const app = express()
 
 app.listen(3333, () => {
+    console.log(process.env.HELLO)
     console.log('Server is up and running!')
 })
 
